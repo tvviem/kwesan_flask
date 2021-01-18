2a96b4799eb1ffa0300e42e1f9e6839650762c35
viem trieu <trieuvinhviem@gmail.com>
2021-01-18 23:03:18 +0700

    Design models detail for app

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from . import db
-import enum
+import enum, datetime
+
 
 class RoleType(enum.Enum):
     ADMI = "Administrator"
     STUD = "Student"
     LECT = "Lecturer"
 
-class User(db.Model):
-    """Data model for user accounts."""
 
+class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.Unicode(15), index=True, nullable=False)
@@ -18,9 +18,16 @@ class User(db.Model):
     password = db.Column(db.String(60), index=False, nullable=False)
     major = db.Column(db.Unicode(50), index=False, nullable=False)
     aboutuser = db.Column(db.Unicode(100), index=False, nullable=True)
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now()
+    )
     role = db.Column(db.Enum(RoleType), index=True, nullable=False)
+    questions = db.relationship("Question", backref="users", lazy=True)
 
-    def __init__(self, firstname, lastname, username, email, password, major, aboutuser, role):
+    def __init__(
+        self, firstname, lastname, username, email, password, major, aboutuser, role
+    ):
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
@@ -32,3 +39,63 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User {} {}>".format(self.username, self.role)
+
+
+userAnswer = db.Table(
+    "useranswers",
+    db.Column("userid", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("questionid", db.Integer, db.ForeignKey("questions.id"), primary_key=True),
+    db.Column(
+        "questionsetid", db.Integer, db.ForeignKey("questionsets.id"), primary_key=True
+    ),
+    db.Column("choice_id_of_user", db.Integer, nullable=True),
+    db.Column("corrected", db.Boolean, nullable=True),
+)
+
+
+class Question(db.Model):
+    __tablename__ = "questions"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Unicode, nullable=False)
+    field = db.Column(db.Unicode, nullable=False)
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now()
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    choices = db.relationship("Ref_Choices", backref="questions", lazy=True)
+    useranswers = db.relationship(
+        "User",
+        secondary=userAnswer,
+        lazy="subquery",
+        backref=db.backref("questions", lazy=True),
+    )
+
+
+class Choice(db.Model):
+    __tablename__ = "choices"
+    id = db.Column(db.Integer, primary_key=True)
+    choice_content = db.Column(db.Unicode, nullable=False, index=True)
+    is_correct = db.Column(db.Boolean, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey("questions.id"), nullable=False)
+
+
+class QuestionSet(db.Model):
+    __tablename__ = "questionsets"
+    id = db.Column(db.Integer, primary_key=True)
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    num_of_question = db.Column(db.SmallInteger, nullable=False)
+    list_question_id = db.Column(db.String(100), nullable=False)
+    duration = db.Column(db.SmallInteger, nullable=False)
+    active_code = db.Column(db.String(64), nullable=False)
+
+
+userParticipate = db.Table(
+    "participates",
+    db.Column("userid", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column(
+        "questionsetid", db.Integer, db.ForeignKey("questionsets.id"), primary_key=True
+    ),
+    db.Column("started_time", db.DateTime, server_default=db.func.now()),
+    db.Column("finished_time", db.DateTime, nullable=True),
+)
